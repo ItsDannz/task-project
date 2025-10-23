b3f8e2d59b3ac499d2e216fbc6270693711a6044
Z <atshiroo@gmail.com>
2025-10-23 17:40:55 +0700

    Update TaskList.php

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TaskList extends Model
 {
-    protected $table = 'lists'; // ✅ tabel sebenarnya
+    protected $table = 'lists';
 
     protected $fillable = [
         'title',
